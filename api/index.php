a42bcc59d5c7bff5605c61431fe56a41de894abd
wemsx <3551697397@qq.com>
2022-06-05 11:11:16 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,39 +1,34 @@
 <?php
-/*
-* Oneword Open source project
-* Version: 1.0
-* Url:https://github.com/VikACG/oneword
-* filename: index.php
-* function: OneWord Main file
-* Author @Zuoyi Chang
-* Copyright © 2019 Vikasu Inc.
-*/
-$path = dirname(__FILE__);
-$file = file($path."/wl.txt");
-
-//Randomly read a line
-$arr  = mt_rand( 0, count( $file ) - 1 );
-$content  = trim($file[$arr]);
-
-//Encoding judgment for outputting the corresponding response header code
-if (isset($_GET['charset']) && !empty($_GET['charset'])) {
-    $charset = $_GET['charset'];
-    if (strcasecmp($charset,"gbk") == 0 ) {
-        $content = mb_convert_encoding($content,'gbk', 'utf-8');
+//存有图片链接的文件名img.txt
+$filename = "wl.txt";
+if(!file_exists($filename)){
+    die('文件不存在');
+}
+ 
+//从文本获取链接
+$pics = [];
+$fs = fopen($filename, "r");
+while(!feof($fs)){
+    $line=trim(fgets($fs));
+    if($line!=''){
+        array_push($pics, $line);
     }
-} else {
-    $charset = 'utf-8';
 }
-
-//Response header output,support cross-domain
-header("Content-Type: text/html; charset=$charset");
-header("Access-Control-Allow-Origin: *");
-
-//Formatted judgment,output js or plain text
-if ($_GET['format'] === 'js') {
-    echo "function oneword(){document.write('" . $content ."');}";
-} elseif ($_GET['format'] === 'img'){
-    echo "document.write("<img src="" . $content .""height='' weight='' alt='' />");";
-}else {
-    echo $content;
+ 
+//从数组随机获取链接
+$pic = $pics[array_rand($pics)];
+ 
+//返回指定格式
+$type=$_GET['type'];
+switch($type){
+ 
+//JSON返回
+case 'json':
+    header('Content-type:text/json');
+    die(json_encode(['pic'=>$pic]));
+ 
+default:
+    die(header("Location: $pic"));
 }
+ 
+?>
